8630f715e4cdbd9c874971c97d013aad00d128b2
Abdiwak Amsalu <140180098+abdiwak-34@users.noreply.github.com>
2024-12-16 10:40:22 -0800

    Time: 10 ms (5.92%) | Memory: 17.2 MB (28.27%) - LeetSync

--- 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.py
+++ 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.py
@@ -1,9 +1,14 @@
 class Solution:
     def getFinalState(self, nums: List[int], k: int, multiplier: int) -> List[int]:
-        for i in range(k):
-            mini = 0
-            for j in range(len(nums)):
-                if nums[j] < nums[mini]:
-                    mini = j
-            nums[mini] *= multiplier
+        heap = []
+        for ind, val in enumerate(nums):
+            heapq.heappush(heap, (val,ind))
+        while k > 0:
+            value, indx = heapq.heappop(heap)
+            value *= multiplier
+            heapq.heappush(heap,(value,indx))
+            k -= 1
+        while heap:
+            val, ind = heapq.heappop(heap)
+            nums[ind] = val
         return nums
