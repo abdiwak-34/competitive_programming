ee2db72f8a516dd58abdb5d393ccc28d0769b1b5
Abdiwak Amsalu <140180098+abdiwak-34@users.noreply.github.com>
2025-02-05 21:23:58 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (28.17%) - LeetSync

---
+++ 722-remove-comments/remove-comments.py
@@ -0,0 +1,41 @@
+class Solution:
+    def removeComments(self, source: List[str]) -> List[str]:
+        '''
+        "/*Test program */"
+        "int main()"
+        "{ "
+        "  // variable declaration "
+        "int a, b, c;"
+        "/* This is a test"
+        "   multiline  "
+        "   comment for "
+        "   testing */"
+        "a = b + c;"
+        "}"
+        '''
+        ans =[]
+        code = []
+        block = False
+        for line in source:
+            i = 0 
+            while i < len(line):
+                if block:
+                    if i + 1 < len(line) and line[i:i+2] == "*/":
+                        block = False
+                        i += 1
+                else:
+                    if i + 1 < len(line) and line[i:i+2] == "/*":
+                        block = True
+                        i += 1
+                    elif i + 1 < len(line) and line[i:i+2] == "//":
+                        break
+                    else:
+                        code.append(line[i])
+                
+                i += 1
+
+            s = ''.join(code)
+            if not block and s:
+                ans.append(s)
+                code.clear()
+        return ans
