8db5dd988d5d04d7097678f65c27212101ee2ce5
Abdiwak Amsalu <140180098+abdiwak-34@users.noreply.github.com>
2025-02-14 10:51:52 +0300

    Time: 634 ms (7.13%) | Memory: 18.5 MB (8.00%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        count_s = defaultdict(int)
+        count_t = Counter(t)
+        def is_substring(count_t, count_s):
+            for key in count_t:
+                if count_t[key] > count_s[key]:
+                    return False
+            return True
+        
+        left = right = 0
+        window =''
+        while left <= right and right < len(s):
+            count_s[s[right]] += 1
+            while is_substring(count_t, count_s):
+                if not window or len(window) > len(s[left:right+1]):
+                    window = s[left:right+1]
+                count_s[s[left]] -= 1
+                left +=1
+            right += 1
+        return window
