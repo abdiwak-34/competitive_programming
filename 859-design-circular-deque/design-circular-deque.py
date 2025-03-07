7da2bac4e809ce945d504b238da0aa48f8e6d0c8
Abdiwak Amsalu <140180098+abdiwak-34@users.noreply.github.com>
2025-03-07 17:29:19 +0300

    Time: 3 ms (87.75%) | Memory: 18.5 MB (75.62%) - LeetSync

--- 859-design-circular-deque/design-circular-deque.py
+++ 859-design-circular-deque/design-circular-deque.py
@@ -2,42 +2,44 @@ class MyCircularDeque:
 
     def __init__(self, k: int):
         self.k = k
-        self.queue = deque()
+        self.queue = deque()        
 
     def insertFront(self, value: int) -> bool:
-        if len(self.queue) < self.k:
-            self.queue.insert(0,value)
-            return True
-        return False        
+        if len(self.queue) == self.k:
+            return False
+        self.queue.appendleft(value)
+        return True
 
     def insertLast(self, value: int) -> bool:
-        if len(self.queue) < self.k:
-            self.queue.append(value)
-            return True
-        return False
+        if len(self.queue) == self.k:
+            return False
+        self.queue.append(value)
+        return True
+
     def deleteFront(self) -> bool:
-        if self.queue:
-            self.queue.popleft()
-            return True
-        return False
+        if not self.queue:
+            return False
+        self.queue.popleft()
+        return True
+
     def deleteLast(self) -> bool:
-        if self.queue:
-            self.queue.pop()
-            return True
-        return False
+        if not self.queue:
+            return False
+        self.queue.pop()
+        return True        
 
     def getFront(self) -> int:
-        if self.queue:
-            return self.queue[0]
-        return -1
+        if not self.queue:
+            return -1
+        return self.queue[0]
 
     def getRear(self) -> int:
-        if self.queue:
-            return self.queue[-1]
-        return -1
+        if not self.queue:
+            return -1
+        return self.queue[-1]
 
     def isEmpty(self) -> bool:
-        return True if not self.queue else False
+        return len(self.queue) == 0
 
     def isFull(self) -> bool:
         return len(self.queue) == self.k
