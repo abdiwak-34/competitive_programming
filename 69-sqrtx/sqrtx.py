a6e040939a954943579b83097d453d4127a5e995
Abdiwak Amsalu <140180098+abdiwak-34@users.noreply.github.com>
2025-03-24 09:18:20 -0700

    Time: 4 ms (40.02%) | Memory: 17.9 MB (32.83%) - LeetSync

---
+++ 69-sqrtx/sqrtx.py
@@ -0,0 +1,13 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        best = 0
+        left, right = 0, x
+        while left <= right:
+            mid = (left+right)//2
+
+            if mid * mid <= x:
+                best = mid
+                left = mid +1
+            elif mid * mid > x:
+                right = mid-1
+        return best
