5697ff376d8616ff6e929c92bd115e85ee2d1656
Abdiwak Amsalu <140180098+abdiwak-34@users.noreply.github.com>
2025-03-28 11:43:13 -0700

    Time: 594 ms (64.52%) | Memory: 25.5 MB (44.63%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -1,14 +1,27 @@
-import heapq
 class Solution:
+    def merge(self, left_half,right_half) -> List[int]:
+        left, right = 0, 0
+        merged = []
+        while left < len(left_half) and right < len(right_half):
+            if left_half[left] <= right_half[right]:
+                merged.append(left_half[left])
+                left += 1
+            else:
+                merged.append(right_half[right])
+                right += 1
+        
+        merged.extend(left_half[left:])
+        merged.extend(right_half[right:])
+        return merged
+    
+    def mergesort(self, left, right, arr):
+        if left == right:
+            return [arr[left]]
+        mid = (left+right)//2
+        left_arr = self.mergesort(left,mid,arr)
+        right_arr = self.mergesort(mid+1,right,arr)
+        return self.merge(left_arr, right_arr)
+
     def sortArray(self, nums: List[int]) -> List[int]:
-        n = max(nums)
-        m = abs(min(nums))
-        count = [0] * (n+m+1)
-        for num in nums:
-            count[num+m] += 1
-        target = 0
-        for ind, val in enumerate(count):
-            for i in range(val):
-                nums[target] = ind - m
-                target += 1
-        return nums
+        left, right = 0, len(nums)-1
+        return self.mergesort(left,right, nums)
