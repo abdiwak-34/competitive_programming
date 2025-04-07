067a54836e6d5a89ad433e14849e2f58f1dffa7c
Abdiwak Amsalu <140180098+abdiwak-34@users.noreply.github.com>
2025-04-07 08:29:18 -0700

    Time: 149 ms (20.44%) | Memory: 18.4 MB (43.97%) - LeetSync

--- 463-island-perimeter/island-perimeter.py
+++ 463-island-perimeter/island-perimeter.py
@@ -2,14 +2,20 @@ class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
         def inbound(row, col):
             return (0 <= row < len(grid) and 0 <= col < len(grid[0]))
-        dir = [[-1,0], [0,-1], [1,0], [0,1]]
         parameter = 0
+
+        def count(i,j):
+            nonlocal parameter
+            dir = [[-1,0], [0,-1], [1,0], [0,1]]
+            for r,c in dir:
+                if inbound(i+r, j+c) and grid[i+r][j+c] == 1:
+                    continue
+                else:
+                    parameter += 1
+        
+
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == 1:
-                    for r,c in dir:
-                        if inbound(i+r, j+c) and grid[i+r][j+c] == 1:
-                            continue
-                        else:
-                            parameter += 1
+                    count(i,j)
         return parameter
