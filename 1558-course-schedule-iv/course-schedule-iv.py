37fc956dcadd0a0758e722b8d4c570b44792dba3
Abdiwak Amsalu <140180098+abdiwak-34@users.noreply.github.com>
2025-06-19 11:58:17 -0700

    Time: 428 ms (23.00%) | Memory: 20.4 MB (83.28%) - LeetSync

---
+++ 1558-course-schedule-iv/course-schedule-iv.py
@@ -0,0 +1,13 @@
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        reachable = [[False] * numCourses for _ in range(numCourses)]
+        for u, v in prerequisites:
+            reachable[u][v] = True
+
+        for k in range(numCourses):
+            for i in range(numCourses):
+                for j in range(numCourses):
+                    if reachable[i][k] and reachable[k][j]:
+                        reachable[i][j] = True
+
+        return [reachable[u][v] for u, v in queries]
