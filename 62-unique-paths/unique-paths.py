7b03de95794ff9fe9e2b74cc3e7fa83caae2d0f2
Abdiwak Amsalu <140180098+abdiwak-34@users.noreply.github.com>
2025-10-01 00:58:37 -0700

    Time: 0 ms (100.00%) | Memory: 17.6 MB (98.34%) - LeetSync

---
+++ 62-unique-paths/unique-paths.py
@@ -0,0 +1,13 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[1 for i in range(m)] for j in range(n)]
+
+        for i in range(n):
+            for j in range(m):
+                if i-1 >=0 and j-1 >= 0:
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
+                elif i-1 >= 0:
+                    dp[i][j] = dp[i-1][j]
+                else:
+                    dp[i][j] = dp[i][j-1]
+        return dp[n-1][m-1]
