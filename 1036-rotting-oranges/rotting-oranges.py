f59d508b6672b9afb2d7b22f073182b485f95448
Abdiwak Amsalu <140180098+abdiwak-34@users.noreply.github.com>
2025-10-24 08:05:21 -0700

    Time: 4 ms (46.94%) | Memory: 18.1 MB (5.50%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -3,26 +3,29 @@ class Solution:
         row, col = len(grid), len(grid[0])
         fresh = 0
         queue = deque()
+        time = 0
         def inbound(ro, co):
             return 0 <= ro < row and 0 <= co < col
 
-        for r in range(row):
-            for c in range(col):
-                if grid[r][c] == 2:
-                    queue.append((r, c))
-                if grid[r][c] == 1:
+        for i in range(row):
+            for j in range(col):
+                if grid[i][j] == 2:
+                    queue.append((i,j))
+                elif grid[i][j] == 1:
                     fresh += 1
-        dirn = [(-1,0),(0,-1),(1,0),(0,1)]
-        time = 0
-        flag =True
-        while queue and fresh > 0:
-            for _ in range(len(queue)):
-                rows, cols = queue.popleft()
-                for a,b in dirn:
-                    if inbound(rows+a, cols+b) and grid[rows+a][cols+b] == 1:
-                        grid[rows+a][cols+b] = 2
+        dirn = [(0,-1),(1,0),(-1,0), (0,1)]
+        while queue:
+            new_rotten = 0
+            for i in range(len(queue)):
+                row1, col1 = queue.popleft()
+                for dr, dc in dirn:
+                    nr, nc = row1+dr, col1+dc
+                    if inbound(nr, nc) and grid[nr][nc] == 1:
+                        queue.append((nr,nc))
+                        grid[nr][nc] = 2
                         fresh -= 1
-                        queue.append((rows+a, cols+b))
-            time += 1
+                        new_rotten += 1
 
-        return time if fresh == 0 else -1
+            if new_rotten > 0:
+                time += 1
+        return time if not fresh else -1
