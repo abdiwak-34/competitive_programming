5b4fdae751b2976d4790425a60eba34c02a4f24b
Abdiwak Amsalu <140180098+abdiwak-34@users.noreply.github.com>
2025-10-24 07:23:41 -0700

    Time: 256 ms (43.51%) | Memory: 20 MB (95.37%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,23 +1,31 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        visted = [[False for _ in range(len(grid[0]))] for i in range(len(grid))]
+        m, n = len(grid), len(grid[0])
 
-        def inbound(row,col):
-            return 0 <= row < len(grid) and 0 <= col < len(grid[0])
-
-        def dfs(row,col):
-            dir = [(-1,0),(0,-1),(1,0),(0,1)]
-            visted[row][col] = True
-            
-            for r,c in dir:
-                if inbound(r+row, c+col) and grid[r+row][c+col] == "1" and not visted[r+row][c+col]:
-                    dfs(r+row, c+col)
+        def inbound(r,c):
+            return 0 <= r < m and 0 <= c < n
+        if not grid or not grid[0]:
+            return 0
         
-        count = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if not visted[i][j] and grid[i][j] == "1":
-                    dfs(i,j)
-                    count += 1
-         
-        return count
+        islands = 0
+
+        def bfs(r, c):
+            queue = deque()
+            dirn = [(-1,0), (0,-1), (1,0), (0,1)]
+            grid[r][c] = "0"
+            queue.append((r,c))
+
+            while queue:
+                row, col = queue.popleft()
+                for dr, dc in dirn:
+                    nr, nc = row + dr, col + dc
+                    if inbound(nr, nc) and grid[nr][nc] == '1':
+                        queue.append((nr, nc))
+                        grid[nr][nc] = '0'
+                
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':
+                    bfs(i, j)
+                    islands += 1
+        return islands
