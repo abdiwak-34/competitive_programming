af160d1f46b09652de72ef1e1dd979d60c468705
Abdiwak Amsalu <140180098+abdiwak-34@users.noreply.github.com>
2025-10-26 22:51:29 -0700

    Time: 167 ms (47.25%) | Memory: 19.3 MB (86.51%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.py
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.py
@@ -1,21 +1,26 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
-        def inbound(row, col):
-            return 0 <= row < len(matrix) and 0 <= col < len(matrix[0])
-        @cache
-        def dfs(r, c):
-            res =1
-            dirn = [(0,1),(1,0),(-1,0),(0,-1)]
-            for dx, dy in dirn:
-                x = r+dx
-                y = c+ dy
-                if inbound(x,y) and matrix[x][y] > matrix[r][c]:
-                    res = max(res, 1+dfs(x,y))
-            return res
+        n, m = len(matrix), len(matrix[0])
 
-        height = 1
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                height = max(height, dfs(i,j))
-        
-        return height
+        def inbound(ro, co):
+            return 0 <= ro < n and 0 <= co < m
+
+        memo = [[-1] * m for _ in range(n)]
+        def findlong(r, c):
+            if memo[r][c] != -1:
+                return memo[r][c]
+            dirn = [(-1,0), (1,0), (0,-1), (0,1)]
+            max_path = 1
+
+            for dr, dc in dirn:
+                nr, nc = r+dr, c+dc
+                if inbound(nr, nc) and matrix[r][c] < matrix[nr][nc]:
+                    max_path = max(max_path, 1 + findlong(nr, nc))
+                
+            memo[r][c] = max_path
+            return max_path
+        ans = 0
+        for i in range(n):
+            for j in range(m):
+                ans = max(findlong(i, j), ans)
+        return ans
